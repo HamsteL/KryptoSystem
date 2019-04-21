e21c74dc1c45a1b9c0fdc3cfa8cced4f8eb147c4
hamstel <hamstel16@gmail.com>
2019-04-21 12:32:33 +0300

    Clear Code

--- KryptoSystem/EncDecForm.cs
+++ KryptoSystem/EncDecForm.cs
@@ -1,35 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KryptoSystem
 {
     public partial class EncDecForm : Form
     {
-        // Declare CspParmeters and RsaCryptoServiceProvider
-        // objects with global scope of your Form class.
         CspParameters cspp = new CspParameters();
         RSACryptoServiceProvider rsa;
 
-        // Path variables for source, encryption, and
-        // decryption folders. Must end with a backslash.
         const string EncrFolder = @"Encrypt\";
         const string DecrFolder = @"Decrypt\";
         const string SrcFolder = @"docs\";
+        const string PubKeyFile = "";
 
-        // Public key file
-        const string PubKeyFile = @"encrypt\rsaPublicKey.txt";
-
-        // Key container name for
-        // private/public key value pair.
         const string keyName = "Key01";
 
         public EncDecForm()
@@ -43,8 +28,6 @@ namespace KryptoSystem
                 MessageBox.Show("Key not set.");
             else
             {
-
-                // Display a dialog box to select a file to encrypt.
                 openFileDialog1.InitialDirectory = SrcFolder;
                 if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
@@ -52,7 +35,7 @@ namespace KryptoSystem
                     if (fName != null)
                     {
                         FileInfo fInfo = new FileInfo(fName);
-                        // Pass the file name without the path.
+
                         string name = fInfo.FullName;
                         EncryptFile(name);
                     }
@@ -78,10 +61,10 @@ namespace KryptoSystem
             openFileDialog1.InitialDirectory = SrcFolder;
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                    F = openFileDialog1.FileName;
-                    FileInfo fInfo = new FileInfo(F);
-                    // Pass the file name without the path.
-                    string PubKeyFile = fInfo.FullName; 
+                F = openFileDialog1.FileName;
+                FileInfo fInfo = new FileInfo(F);
+
+                string PubKeyFile = fInfo.FullName;
             }
 
             StreamReader sr = new StreamReader(PubKeyFile);
@@ -116,7 +99,7 @@ namespace KryptoSystem
                 MessageBox.Show("Key not set.");
             else
             {
-                // Display a dialog box to select the encrypted file.
+
                 openFileDialog2.InitialDirectory = EncrFolder;
                 if (openFileDialog2.ShowDialog() == DialogResult.OK)
                 {
@@ -133,7 +116,7 @@ namespace KryptoSystem
 
         private void buttonCreateAsmKeys_Click(object sender, EventArgs e)
         {
-            // Stores a key pair in the key container.
+
             cspp.KeyContainerName = keyName;
             rsa = new RSACryptoServiceProvider(cspp);
             rsa.PersistKeyInCsp = true;
@@ -145,23 +128,13 @@ namespace KryptoSystem
 
         private void EncryptFile(string inFile)
         {
-
-            // Create instance of Rijndael for
-            // symetric encryption of the data.
             RijndaelManaged rjndl = new RijndaelManaged();
             rjndl.KeySize = 256;
             rjndl.BlockSize = 256;
             rjndl.Mode = CipherMode.CBC;
             ICryptoTransform transform = rjndl.CreateEncryptor();
 
-            // Use RSACryptoServiceProvider to
-            // enrypt the Rijndael key.
-            // rsa is previously instantiated: 
-            //    rsa = new RSACryptoServiceProvider(cspp);
             byte[] keyEncrypted = rsa.Encrypt(rjndl.Key, false);
-
-            // Create byte arrays to contain
-            // the length values of the key and IV.
             byte[] LenK = new byte[4];
             byte[] LenIV = new byte[4];
 
@@ -170,16 +143,8 @@ namespace KryptoSystem
             int lIV = rjndl.IV.Length;
             LenIV = BitConverter.GetBytes(lIV);
 
-            // Write the following to the FileStream
-            // for the encrypted file (outFs):
-            // - length of the key
-            // - length of the IV
-            // - ecrypted key
-            // - the IV
-            // - the encrypted cipher content
-
             int startFileName = inFile.LastIndexOf("\\") + 1;
-            // Change the file's extension to ".enc"
+
             string outFile = EncrFolder + inFile.Substring(startFileName, inFile.LastIndexOf(".") - startFileName) + ".enc";
 
             using (FileStream outFs = new FileStream(outFile, FileMode.Create))
@@ -190,18 +155,12 @@ namespace KryptoSystem
                 outFs.Write(keyEncrypted, 0, lKey);
                 outFs.Write(rjndl.IV, 0, lIV);
 
-                // Now write the cipher text using
-                // a CryptoStream for encrypting.
+
+
                 using (CryptoStream outStreamEncrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
                 {
-
-                    // By encrypting a chunk at
-                    // a time, you can save memory
-                    // and accommodate large files.
                     int count = 0;
                     int offset = 0;
-
-                    // blockSizeBytes can be any arbitrary size.
                     int blockSizeBytes = rjndl.BlockSize / 8;
                     byte[] data = new byte[blockSizeBytes];
                     int bytesRead = 0;
@@ -228,87 +187,48 @@ namespace KryptoSystem
 
         private void DecryptFile(string inFile)
         {
-
-            // Create instance of Rijndael for
-            // symetric decryption of the data.
             RijndaelManaged rjndl = new RijndaelManaged();
             rjndl.KeySize = 256;
             rjndl.BlockSize = 256;
             rjndl.Mode = CipherMode.CBC;
 
-            // Create byte arrays to get the length of
-            // the encrypted key and IV.
-            // These values were stored as 4 bytes each
-            // at the beginning of the encrypted package.
             byte[] LenK = new byte[4];
             byte[] LenIV = new byte[4];
 
-            // Consruct the file name for the decrypted file.
             string outFile = DecrFolder + inFile.Substring(0, inFile.LastIndexOf(".")) + ".txt";
 
-            // Use FileStream objects to read the encrypted
-            // file (inFs) and save the decrypted file (outFs).
             using (FileStream inFs = new FileStream(EncrFolder + inFile, FileMode.Open))
             {
-
                 inFs.Seek(0, SeekOrigin.Begin);
                 inFs.Seek(0, SeekOrigin.Begin);
                 inFs.Read(LenK, 0, 3);
                 inFs.Seek(4, SeekOrigin.Begin);
                 inFs.Read(LenIV, 0, 3);
 
-                // Convert the lengths to integer values.
                 int lenK = BitConverter.ToInt32(LenK, 0);
                 int lenIV = BitConverter.ToInt32(LenIV, 0);
-
-                // Determine the start postition of
-                // the ciphter text (startC)
-                // and its length(lenC).
                 int startC = lenK + lenIV + 8;
                 int lenC = (int)inFs.Length - startC;
-
-                // Create the byte arrays for
-                // the encrypted Rijndael key,
-                // the IV, and the cipher text.
                 byte[] KeyEncrypted = new byte[lenK];
                 byte[] IV = new byte[lenIV];
 
-                // Extract the key and IV
-                // starting from index 8
-                // after the length values.
                 inFs.Seek(8, SeekOrigin.Begin);
                 inFs.Read(KeyEncrypted, 0, lenK);
                 inFs.Seek(8 + lenK, SeekOrigin.Begin);
                 inFs.Read(IV, 0, lenIV);
                 Directory.CreateDirectory(DecrFolder);
-                // Use RSACryptoServiceProvider
-                // to decrypt the Rijndael key.
+
                 byte[] KeyDecrypted = rsa.Decrypt(KeyEncrypted, false);
 
-                // Decrypt the key.
                 ICryptoTransform transform = rjndl.CreateDecryptor(KeyDecrypted, IV);
 
-                // Decrypt the cipher text from
-                // from the FileSteam of the encrypted
-                // file (inFs) into the FileStream
-                // for the decrypted file (outFs).
                 using (FileStream outFs = new FileStream(outFile, FileMode.Create))
                 {
-
                     int count = 0;
                     int offset = 0;
-
-                    // blockSizeBytes can be any arbitrary size.
                     int blockSizeBytes = rjndl.BlockSize / 8;
                     byte[] data = new byte[blockSizeBytes];
 
-
-                    // By decrypting a chunk a time,
-                    // you can save memory and
-                    // accommodate large files.
-
-                    // Start at the beginning
-                    // of the cipher text.
                     inFs.Seek(startC, SeekOrigin.Begin);
                     using (CryptoStream outStreamDecrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
                     {
